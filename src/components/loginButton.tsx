c96505621b8cc043689063a4f9a67cf1f855dcd9
MWANGIKENNEDY <kenmwan75@gmail.com>
2025-02-02 20:45:12 +0300

    fixed undefined type

--- src/components/loginButton.tsx
+++ src/components/loginButton.tsx
@@ -1,6 +1,5 @@
-"use client"
+"use client";
 import React from "react";
-
 import { SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 
@@ -9,13 +8,13 @@ const LoginButton = ({
   setOpen,
 }: {
   open?: boolean;
-  setOpen: (open: boolean) => void;
+  setOpen?: (open: boolean) => void;
 }) => {
   return (
     <SignedOut>
-      <li onClick={() => setOpen(!open)}>
+      <li onClick={() => setOpen && setOpen(!open)}>
         <Link href="/login">
-          <button className=" px-4 py-2 bg-blue-400 rounded-3xl"> Login</button>
+          <button className="px-4 py-2 bg-blue-400 rounded-3xl">Login</button>
         </Link>
       </li>
     </SignedOut>
