2be543825a6c57a7cbc715c8384ef02c68969073
MWANGIKENNEDY <kenmwan75@gmail.com>
2025-02-02 21:12:58 +0300

    add fix for react-quill

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,30 +1,33 @@
 "use client";
+
 import React from "react";
+import dynamic from "next/dynamic";
 import "react-quill-new/dist/quill.snow.css";
-import ReactQuill from "react-quill-new";
+
+// Dynamically import ReactQuill without SSR
+const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
 
 const WritePostPage = () => {
-
   return (
-    <div className=" space-y-6">
-      <h1 className=" font-light  text-xl text-gray-600">Create a new post</h1>
+    <div className="space-y-6">
+      <h1 className="font-light text-xl text-gray-600">Create a new post</h1>
 
-      <form action="" className=" space-y-7 p-2">
+      <form action="" className="space-y-7 p-2">
         <button className="bg-white px-4 py-2 rounded text-sm shadow-md">
           Add a cover image
         </button>
-        <h1 className=" text-3xl lg:text-5xl font-bold text-gray-400">
+        <h1 className="text-3xl lg:text-5xl font-bold text-gray-400">
           My Awesome Story
         </h1>
 
-        <div className=" flex items-center gap-5">
-          <label htmlFor="cat" className=" text-sm text-gray-700">
+        <div className="flex items-center gap-5">
+          <label htmlFor="cat" className="text-sm text-gray-700">
             Choose a category:
           </label>
           <select
             name="cat"
-            className=" w-[200px] px-3 py-3 rounded-lg  shadow-xl outline-none border-none"
-            id=""
+            className="w-[200px] px-3 py-3 rounded-lg shadow-xl outline-none border-none"
+            id="cat"
           >
             <option value="general">General</option>
             <option value="web-design">Web Design</option>
@@ -36,20 +39,20 @@ const WritePostPage = () => {
         </div>
 
         <textarea
-          name=""
-          className=" block w-full rounded-lg shadow-xl p-4 outline-none border-none"
+          className="block w-full rounded-lg shadow-xl p-4 outline-none border-none"
           placeholder="A Short Description"
-          id=""
           cols={30}
           rows={3}
         ></textarea>
+
+        {/* ReactQuill Editor */}
         <ReactQuill
           theme="snow"
-        
-          className=" shadow-lg"
-          style={{ minHeight: "500px", background: "white ", border: "none" }}
+          className="shadow-lg"
+          style={{ minHeight: "500px", background: "white", border: "none" }}
         />
-        <button className=" bg-blue-500 text-white px-4 py-2 rounded-lg">
+
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">
           Send
         </button>
       </form>
